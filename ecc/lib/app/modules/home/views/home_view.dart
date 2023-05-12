97f04b4c2e84aba094d2d198360f010599cca902
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2023-05-11 23:01:54 -0300

    boom passando

--- ecc/lib/app/modules/home/views/home_view.dart
+++ ecc/lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boom_menu_new/flutter_boom_menu_new.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
-
-import 'package:flutter_boom_menu/flutter_boom_menu.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -13,6 +12,7 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: buildBoomMenu(),
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
@@ -35,46 +35,53 @@ class HomeView extends GetView<HomeController> {
         onClose: () => print('DIAL CLOSED'),
         scrollVisible: scrollVisible,
         overlayColor: Colors.black,
+        elevation: 10,
         overlayOpacity: 0.7,
         children: [
-          MenuItem(
+          MenuItemModel(
 //          child: Icon(Icons.accessibility, color: Colors.black, size: 40,),
-            child:
-                Image.asset('assets/logout_icon.png', color: Colors.grey[850]),
+            child: Image.asset('assets/images/logout_icon.png',
+                color: Colors.grey[850]),
             title: "Logout",
-            titleColor: Colors.grey[850],
+            titleColor: Colors.grey[850]!,
             subtitle: "Lorem ipsum dolor sit amet, consectetur",
-            subTitleColor: Colors.grey[850],
-            backgroundColor: Colors.grey[50],
+            subTitleColor: Colors.grey[850]!,
+            backgroundColor: Colors.grey[50]!,
             onTap: () => print('THIRD CHILD'),
+            elevation: 10,
           ),
-          MenuItem(
-            child: Image.asset('assets/schemes_icon.png', color: Colors.white),
+          MenuItemModel(
+            child: Image.asset('assets/images/schemes_icon.png',
+                color: Colors.white),
             title: "List",
             titleColor: Colors.white,
             subtitle: "Lorem ipsum dolor sit amet, consectetur",
             subTitleColor: Colors.white,
             backgroundColor: Colors.pinkAccent,
             onTap: () => print('FOURTH CHILD'),
+            elevation: 10,
           ),
-          MenuItem(
-            child: Image.asset('assets/customers_icon.png',
+          MenuItemModel(
+            child: Image.asset('assets/images/customers_icon.png',
                 color: Colors.grey[850]),
             title: "Team",
-            titleColor: Colors.grey[850],
+            titleColor: Colors.grey[850]!,
             subtitle: "Lorem ipsum dolor sit amet, consectetur",
-            subTitleColor: Colors.grey[850],
-            backgroundColor: Colors.grey[50],
+            subTitleColor: Colors.grey[850]!,
+            backgroundColor: Colors.grey[50]!,
             onTap: () => print('THIRD CHILD'),
+            elevation: 10,
           ),
-          MenuItem(
-            child: Image.asset('assets/profile_icon.png', color: Colors.white),
+          MenuItemModel(
+            child: Image.asset('assets/images/profile_icon.png',
+                color: Colors.white),
             title: "Profile",
             titleColor: Colors.white,
             subtitle: "Lorem ipsum dolor sit amet, consectetur",
             subTitleColor: Colors.white,
             backgroundColor: Colors.blue,
             onTap: () => print('FOURTH CHILD'),
+            elevation: 10,
           )
         ]);
   }
