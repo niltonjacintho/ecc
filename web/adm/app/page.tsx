79e5698050351d3d6caa19a11996723d55048879
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2023-07-24 12:54:04 -0300

    a

---
+++ web/adm/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-3x1">ECC</h1>
+      </header>
+    </>
+  );
+}
